5d3637b6a3b443580af3911a095dcd210e90c013
Kassim <kassimrooney@gmail.com>
2025-05-18 20:48:33 +0300

    Created  customer and coffe Classes

---
+++ coffee.py
@@ -0,0 +1,12 @@
+class Coffee:
+    def __init__(self, name):
+        if not isinstance(name, str):
+            raise TypeError("Name must be a string.")
+        if len(name) < 3:
+            raise ValueError("Name must be atleast 3 characters long.")
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+    
