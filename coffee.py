e48e1303245c06b0c1ba4fe881ab9ecac74fb8f0
Kassim <kassimrooney@gmail.com>
2025-05-18 21:25:30 +0300

    Completed Coffee class Object relations

--- coffee.py
+++ coffee.py
@@ -1,4 +1,6 @@
 class Coffee:
+    _all = []
+
     def __init__(self, name):
         if not isinstance(name, str):
             raise TypeError("Name must be a string.")
@@ -10,3 +12,15 @@ class Coffee:
     def name(self):
         return self._name
     
+    def orders(self):
+        return[order for order in Order._all if order.coffee == self]
+    
+    def customers(self):
+        return list(set(order.customer for order in self.orders()))
+    
+    def number_of_orders(self):
+        return len(self.orders())
+    
+    def average_price(self):
+        prices = [order.price for order in self.orders()]
+        return sum(prices) / len(prices) if prices else 0 
