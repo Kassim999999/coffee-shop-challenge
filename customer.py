b09c92778fa619fe5a29ffb98f2e8b93e18b69d8
Kassim <kassimrooney@gmail.com>
2025-05-18 21:19:31 +0300

    Completed Customer class Object relations

--- customer.py
+++ customer.py
@@ -1,6 +1,12 @@
+from order import Order
+
 class Customer:
+
+    _all =[]
+
     def __init__(self,name):
         self.name = name
+        Customer._all.append(self)
 
     @property
     def name(self):
@@ -13,3 +19,13 @@ class Customer:
         if not (1 <= len(value) <= 15):
             raise ValueError("Name must be 1 to 15 characters.")
         self._name = value
+
+    def orders(self):
+        return[order for order in Order._all if order.customer == self]
+    
+    def coffees(self):
+        return list(set(order.coffee for order in self.orders()))
+    
+    def create_order(self, coffee, price):
+        return Order(self, coffee, price)
+    
